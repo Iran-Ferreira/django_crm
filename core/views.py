05027fe5b88f4c052e22586e2ebc2179bc7cd6ea
Iran Ferreira <iranferreira712@gmail.com>
2024-04-27 17:17:49 -0300

    feat: new methods login and submit login

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as app_login
+from django.contrib import messages
 
-# Create your views here.
+def login(request):
+    return render(request, "login.html")
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST["username"] 
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password)
+        if user:
+            app_login(request, user)
+            return redirect("index")
+        else:
+            messages.error(request, "Usuário ou senha inválidos. Por favor tente novamente.")
+    return redirect("login")
